8fbe9c7163e28f6c401cf2e94ee1c57cf1bc70b0
Satya Ikyath Varma Dantuluri <dantuluri.s@husky.neu.edu>
2020-05-14 15:21:27 -0400

    Create model.py

---
+++ model.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 14 15:04:06 2020
+
+@author: ikyathvarmadantuluri
+"""
+
+
+import pandas as pd
+from sklearn.metrics import accuracy_score,confusion_matrix
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from xgboost import XGBClassifier
+
+
+train = pd.read_csv('Data/cardio_train.csv',sep=';')
+
+
+train = train.drop(train[(train.height<147) | (train.height>184)].index)
+train = train.drop(train[(train.weight<48) | (train.weight>117)].index)
+
+train.drop_duplicates(inplace=True)
+
+
+s_list = ["age", "height", "weight", "ap_hi", "ap_lo"]
+def standartization(x):
+    x_std = x.copy(deep=True)
+    for column in s_list:
+        x_std[column] = (x_std[column]-x_std[column].mean())/x_std[column].std()
+    return x_std 
+train_std=standartization(train)
+
+train=train_std
+
+
+features=["age", "height", "weight",'ap_hi','ap_lo','smoke','alco','active','cholesterol','gluc']
+
+X_train, X_test, y_train, y_test = train_test_split(train[features], train['cardio'], test_size=0.33, random_state=42)
+
+xgb = XGBClassifier(learning_rate=0.01,n_estimators=500)
+
+
+xgb.fit(X_train,y_train)
+
+
+y_pred_xgb = xgb.predict(X_test)
+
+accuracy = xgb.score(X_test, y_test)
+
+
